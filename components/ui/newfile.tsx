a726d9d852cc09b85241e8d219aeb449f9367ddf
rohitKoktare <rohitkoktarebugatti@gmail.com.com>
2026-04-03 18:41:02 +0530

    normal changes

---
+++ components/ui/newfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewFile: React.FC = () => {
+  return (
+    <div>
+      <h1>This is the normal component for creating a new file.
+      </h1>
+    </div>
+  );
+};
+
+export default NewFile;
